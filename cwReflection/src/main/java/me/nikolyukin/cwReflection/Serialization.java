de0f96415e1902e3f5b3112baa4d764dfc7c8893
Michael <michael.nik999@gmail.com>
2019-02-20 17:12:10 +0300

    add deserialize

--- cwReflection/src/main/java/me/nikolyukin/cwReflection/Serialization.java
+++ cwReflection/src/main/java/me/nikolyukin/cwReflection/Serialization.java
@@ -1,18 +1,21 @@
 package me.nikolyukin.cwReflection;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 
 public class Serialization {
-    void serialize(Object object, OutputStream out) {
-        try (ObjectOutputStream objectOut = new ObjectOutputStream(new BufferedOutputStream(out))) {
+    void serialize(Object object, OutputStream out) throws IOException {
+        try (var objectOut = new ObjectOutputStream(new BufferedOutputStream(out))) {
             Class<?> objectClass = object.getClass();
             for (Field field : objectClass.getDeclaredFields()) {
-                objectOut.writeObject(field);
+//                objectOut.writeObject(field);
                 Class<?> fieldType = field.getType();
                 switch (fieldType.getName()) {
                     case "byte":
@@ -44,13 +47,60 @@ public class Serialization {
                         break;
                 }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         }
     }
 
-    T deserialize(InputStream in, Class<T> class) {
+    <T> T deserialize(InputStream in, Class<T> objectClass) throws IOException {
+        T result = null;
+        try {
+            result = objectClass.getConstructor().newInstance();
+            try (var objectIn = new ObjectInputStream(new BufferedInputStream(in))) {
+                for (Field field : objectClass.getDeclaredFields()) {
+                    Class<?> fieldType = field.getType();
+                    switch (fieldType.getName()) {
+                        case "byte":
+                            field.setByte(result, objectIn.readByte());
+                            break;
+                        case "char":
+                            field.setChar(result, objectIn.readChar());
+                            break;
+                        case "short":
+                            field.setShort(result, objectIn.readShort());
+                            break;
+                        case "int":
+                            field.setInt(result, objectIn.readInt());
+                            break;
+                        case "long":
+                            field.setLong(result, objectIn.readLong());
+                            break;
+                        case "float":
+                            field.setFloat(result, objectIn.readFloat());
+                            break;
+                        case "double":
+                            field.setDouble(result, objectIn.readDouble());
+                            break;
+                        case "boolean":
+                            field.setBoolean(result, objectIn.readBoolean());
+                            break;
+                        default:
+                            field.set(result, objectIn.readObject());
+                            break;
+                    }
+                }
+            } catch (IllegalAccessException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 }
