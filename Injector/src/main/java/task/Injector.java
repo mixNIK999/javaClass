1e62005014a1ec7405745b1a336852abb3cca8ff
Michael <michael.nik999@gmail.com>
2019-03-06 16:03:53 +0300

    init Injector

---
+++ Injector/src/main/java/task/Injector.java
@@ -0,0 +1,18 @@
+package task;
+
+import java.util.List;
+
+
+public class Injector {
+
+    /**
+     * Create and initialize object of `rootClassName` class using classes from
+     * `implementationClassNames` for concrete dependencies.
+     */
+    public static Object initialize(String rootClassName, List<String> implementationClassNames) throws Exception {
+        throw new IllegalStateException();
+
+    }
+
+    private static Object
+}
