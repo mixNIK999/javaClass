d78af011e67740f27469aa3b60ed983b5f232109
Michael <michael.nik999@gmail.com>
2019-04-24 17:16:47 +0300

    firstApp

---
+++ javaFX/src/main/java/me/nikolyukin/FirstApp.java
@@ -0,0 +1,14 @@
+package me.nikolyukin;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+public class FirstApp extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("My First JavaFX App");
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
