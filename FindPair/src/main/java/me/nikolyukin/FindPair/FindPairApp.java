c74c4f13667b06a2a09b9b2b4e527da720a68915
Michael <michael.nik999@gmail.com>
2019-05-29 15:45:10 +0300

    add applicatioCn

---
+++ FindPair/src/main/java/me/nikolyukin/FindPair/FindPairApp.java
@@ -0,0 +1,61 @@
+package me.nikolyukin.FindPair;
+
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+import javafx.stage.Stage;
+
+public class FindPairApp  extends Application {
+    private int turn = 0;
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        var pane = new GridPane();
+        pane.setGridLinesVisible(true);
+
+        int n = 10;
+        for (int i = 0; i < n; i++) {
+            var column = new ColumnConstraints();
+            column.setPercentWidth(100.0/n);
+            pane.getColumnConstraints().add(column);
+
+            var row = new RowConstraints();
+            row.setPercentHeight(100.0/n);
+            pane.getRowConstraints().add(row);
+
+            for (int j = 0; j < n; j++) {
+                pane.add(new XOButton(), i, j);
+            }
+        }
+
+        var scene = new Scene(pane, 300, 300);
+        primaryStage.setTitle("Find Pair");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    private class XOButton extends Button {
+        private final String[] text = {"X", "0"};
+        private boolean wasChanged = false;
+
+        private XOButton() {
+            setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+            setOnAction(value -> {
+                if(!wasChanged) {
+                    setText(text[turn % 2]) ;
+                    turn++;
+                    wasChanged = true;
+                }
+            });
+        }
+    }
+}
+
