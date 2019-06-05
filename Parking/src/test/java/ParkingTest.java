8792110aba3fdc1bce18453fcc0f3cd4fca7c4d1
Michael <michael.nik999@gmail.com>
2019-06-05 16:48:31 +0300

    add tests

---
+++ Parking/src/test/java/ParkingTest.java
@@ -0,0 +1,116 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ParkingTest {
+
+    private Parking parking;
+    private final int max = 3;
+    private ExecutorService pool;
+
+    @BeforeEach
+    void init() {
+        parking = new Parking(max);
+        pool = Executors.newFixedThreadPool(4);
+    }
+
+    @AfterEach
+    void shutdown() {
+        pool.shutdown();
+    }
+
+    @Test
+    void enterOne() {
+        assertTrue(parking.enter());
+    }
+
+    @Test
+    void enterMoreThanMax() {
+        for (int i = 0; i < max; i++) {
+            assertTrue(parking.enter());
+        }
+        assertFalse(parking.enter());
+    }
+
+    @Test
+    void enterMoreThanMaxExitAndEnter() {
+        for (int i = 0; i < max; i++) {
+            assertTrue(parking.enter());
+        }
+        assertFalse(parking.enter());
+        assertTrue(parking.exit());
+        assertTrue(parking.enter());
+    }
+
+    @Test
+    void ExitMoreThenEnter() {
+        for (int i = 0; i < max; i++) {
+            assertTrue(parking.enter());
+        }
+        assertFalse(parking.enter());
+        for (int i = 0; i < max; i++) {
+            assertTrue(parking.exit());
+        }
+        assertFalse(parking.exit());
+    }
+
+    @Test
+    void multiTreadEnter() {
+        var car = new Callable<Boolean> (){
+
+            @Override
+            public Boolean call() {
+                return parking.enter();
+            }
+        };
+
+        var futures = new ArrayList<Future<Boolean>>();
+        int cntFalse = max + 1;
+        for (int i = 0; i < max + cntFalse; i++) {
+            futures.add(pool.submit(car));
+        }
+
+        assertEquals(cntFalse, futures.stream().filter(f -> {
+            try {
+                return !f.get();
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException();
+            }
+        }).count());
+    }
+
+    @Test
+    void multiTreadEnterAndExit() {
+        var car = new Callable<Boolean> (){
+
+            @Override
+            public Boolean call() {
+                parking.enter();
+                return parking.exit();
+            }
+        };
+
+        var futures = new ArrayList<Future<Boolean>>();
+
+        for (int i = 0; i < max; i++) {
+            futures.add(pool.submit(car));
+        }
+
+        assertEquals(max, futures.stream().filter(f -> {
+            try {
+                return f.get();
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException();
+            }
+        }).count());
+    }
+}
