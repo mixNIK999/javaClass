5fbb1d9dcaaae47f414201f569261776226d9b4e
Michael <michael.nik999@gmail.com>
2019-06-05 16:48:21 +0300

    add javadoc

--- Parking/src/main/java/Parking.java
+++ Parking/src/main/java/Parking.java
@@ -3,23 +3,48 @@
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.IntBinaryOperator;
 
+/**
+ * lock-free парковка.
+ * Позволяет пускать машины, если есть место, и регестрировать уезжающие машины.
+ */
 public class Parking {
     private final AtomicInteger counter;
-    private int max;
+    private final int max;
+    private final int min;
 
-    private final IntBinaryOperator addWithMax = ((a, b) -> Integer.min(a + b, max));
-    private final IntBinaryOperator subWithMin = ((a, b) -> Integer.max(a - b, 0));
+    private final IntBinaryOperator addWithMax;
+    private final IntBinaryOperator subWithMin;
 
+    /**
+     * Конструктор создает парковку с заданным максимальным количеством мест.
+     * @param max максимальное клличество мест. должно быть >= 0.
+     * @throws IllegalArgumentException если max < 0.
+     */
     public Parking(int max) {
+        if (max < 0) {
+            throw new IllegalArgumentException("max < 0");
+        }
         this.max = max;
-        counter = new AtomicInteger(0);
+        min = 0;
+        addWithMax = ((a, b) -> Integer.min(a + b, max));
+        subWithMin = ((a, b) -> Integer.max(a - b, min));
+        counter = new AtomicInteger(min);
     }
 
+    /**
+     * Метод сообщает машине при въезде, есть ли свободные места.
+     *
+     * @return true, если количество машин на парковке увеличилось.
+     */
     public boolean enter() {
         return counter.getAndAccumulate(1, addWithMax) != max;
-
     }
 
+    /**
+     * Метод регистрирует уезжающие машины.
+     *
+     * @return true, если количество машин на парковке уменьшилось.
+     */
     public boolean exit() {
         return counter.getAndAccumulate(1, subWithMin) != 0;
     }
